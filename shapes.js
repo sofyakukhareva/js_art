43bf6ac86f9e5d20fc9230fd5983aab5f2700f63
sofyakukhareva <s.kuxareva@gmail.com>
2022-05-22 01:19:26 +0300

    update

--- shapes.js
+++ shapes.js
@@ -4,34 +4,34 @@ e=[(_=>{x[i]=cos(v*i)*r;y[i]=sin(v*i)*r;
 u=15;s=2;r=150;x=[];y=[];
 
 setup=_=>{
-createCanvas(windowWidth, windowHeight);
-z=random;c=curveVertex
-m=width/2;n=height/2;
-v=radians(360/u);
-for (i=0;i<u;i++){e[0]();}
-stroke(0,50);
-strokeWeight(.75);
-background(255);
-frameRate(5);
+    createCanvas(windowWidth, windowHeight);
+    z=random;c=curveVertex
+    m=width/2;n=height/2;
+    v=radians(360/u);
+    for (i=0;i<u;i++){e[0]();}
+    stroke(0,50);
+    strokeWeight(.75);
+    background(255);
+    frameRate(5);
 };
 draw=_=>{
-m+=(mouseX-m)*.01;
-n+=(mouseY-n)*.01;
-noFill();
-beginShape();
-c(x[u-1]+m,y[u-1]+n);
-for (i=0;i<u;i++) {e[1]();}
-c(x[0]+m,y[0]+n);
-c(x[1]+m,y[1]+n);
-endShape();
+    m+=(mouseX-m)*.01;
+    n+=(mouseY-n)*.01;
+    noFill();
+    beginShape();
+    c(x[u-1]+m,y[u-1]+n);
+    for (i=0;i<u;i++) {e[1]();}
+    c(x[0]+m,y[0]+n);
+    c(x[1]+m,y[1]+n);
+    endShape();
 }
 
 function mousePressed() {
-m=mouseX;
-n=mouseY;
-//var radius=r*random(0.5, 1);
-for (var i = 0; i < u; i++) {
-x[i]=cos(v*i)*r;
-y[i]=sin(v*i)*r;
-}
+    m=mouseX;
+    n=mouseY;
+    var radius=r*random(0.5, 1);
+    for (var i = 0; i < u; i++) {
+    x[i]=cos(v*i)*r;
+    y[i]=sin(v*i)*r;
+    }
 }
